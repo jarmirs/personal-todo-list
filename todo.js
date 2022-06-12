533a19665077dbf50ab4843b20bab5cba5f779b2
Jarmir Smith <55027346+jarmirs@users.noreply.github.com>
2022-06-12 16:39:54 -0400

    +dom const

--- todo.js
+++ todo.js
@@ -1,1 +1,7 @@
 // window.localStorage.setItem(arg1 , arg2 ); : // function use save todo list stays on browser
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const list_el = document.querySelector("#task");
+  console.log(form);
+});
