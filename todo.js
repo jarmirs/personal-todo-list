8e8d8e6ca99c4c78a716f3521717cafd60593465
Jarmir Smith <55027346+jarmirs@users.noreply.github.com>
2022-06-13 17:44:54 -0400

    fixed js error

--- todo.js
+++ todo.js
@@ -2,7 +2,6 @@
 window.addEventListener("load", () => {
   const form = document.querySelector("#new-task-form");
   const input = document.querySelector("#new-task-input");
-  const list_el = document.querySelector("#tasks");
 
   //   stop from refreshing page
   form.addEventListener("submit", (e) => {
@@ -14,6 +13,7 @@ window.addEventListener("load", () => {
     return;
   });
 });
+const list_el = document.querySelector("#tasks");
 // create Dom noDES
 const task_el = document.createElement("div");
 task_el.classList.add("task");
