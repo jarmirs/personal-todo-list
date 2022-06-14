b51c460114ed044b47785eec379cbd44f7eb3b55
Jarmir Smith <55027346+jarmirs@users.noreply.github.com>
2022-06-14 17:53:18 -0400

    fixed js

--- todo.js
+++ todo.js
@@ -6,7 +6,8 @@ window.addEventListener("load", () => {
   //   stop from refreshing page
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const task = input.value;
+    const task =document.getElementsByClassName("text");
+
     if (!task) {
       alert("Please add a task");
     }
@@ -20,13 +21,13 @@ task_el.classList.add("task");
 
 const task_content_el = document.createElement("div");
 task_content_el.classList.add("content");
-task_content_el.innerText = "task";
+task_content_el.innerText = task;
 
 task_el.appendChild(task_content_el);
 const task_input_el = document.createElement("input");
 task_input_el.classList.add("text");
 task_input_el.type = "text";
-task_input_el.value = " task";
+task_input_el.value =task;
 task_input_el.setAttribute("readonly", "readonly");
 
 task_content_el.appendChild(task_input_el);
